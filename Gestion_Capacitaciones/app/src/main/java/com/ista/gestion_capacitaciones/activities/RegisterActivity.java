5c1e46f8547be512547899396fd686e6810b6e67
ELKEVINQ <71275632+ELKEVINQ@users.noreply.github.com>
2023-04-25 11:04:42 -0500

    primeras clases
    nada

---
+++ Gestion_Capacitaciones/app/src/main/java/com/ista/gestion_capacitaciones/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.ista.gestion_capacitaciones.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ista.gestion_capacitaciones.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
