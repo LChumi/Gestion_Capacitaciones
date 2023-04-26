5fcc7c2f76e729dc19977e4b965c090d85eb7745
LChumi <luischumi.9@gmail.com>
2023-04-26 16:58:47 -0500

    update

---
+++ Gestion_Capacitaciones/app/src/main/java/com/ista/gestion_capacitaciones/constants/ApiUrls.java
@@ -0,0 +1,2 @@
+package com.ista.gestion_capacitaciones.constants;public class ApiUrls {
+}
--- Gestion_Capacitaciones/app/src/main/java/com/ista/gestion_capacitaciones/utils/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.ista.gestion_capacitaciones.utils;
-
-public class Constants {
-    public static final String BASE_URL = "";
-}
