a8869abf6440c74c88bfb44b8c9d886d49b85029
ELKEVINQ <71275632+ELKEVINQ@users.noreply.github.com>
2023-04-27 21:21:13 -0500

    lista curso por lista estudiante
    a

--- Gestion_Capacitaciones/app/src/main/java/com/ista/gestion_capacitaciones/UI/ListaEstudianteActivity.java
+++ Gestion_Capacitaciones/app/src/main/java/com/ista/gestion_capacitaciones/UI/ListaEstudianteActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.ista.gestion_capacitaciones.R;
 
-public class ListaCursoActivity extends AppCompatActivity {
+public class ListaEstudianteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_curso);
+        setContentView(R.layout.activity_lista_estudiante);
     }
 }
