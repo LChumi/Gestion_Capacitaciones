c3d8d6bd72dc963bcfa6574024cf21fb57760128
LChumi <luischumi.9@gmail.com>
2023-05-27 19:53:45 -0500

    update final

--- Gestion_Capacitaciones/app/src/main/java/com/ista/gestion_capacitaciones/constants/ApiUrls.java
+++ Gestion_Capacitaciones/app/src/main/java/com/ista/gestion_capacitaciones/constants/ApiUrls.java
@@ -19,6 +19,6 @@ import java.net.URL;
 
 public class ApiUrls {
 
-    //public static fina String BASE_URL=http://direccion web o ip de servidor
-    public static final String BASE_URL="http://10.0.2.2:8080/";
+    public static final String BASE_URL="http://165.22.182.237:8080/";
+    //public static final String BASE_URL="http://10.0.2.2:8080/";
 }
